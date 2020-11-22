064b88a2d0158d4e913a92c579defe0f55e9d451
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-11-21 22:50:59 -0500

    test(ffi): add basic sys_setre*id() test

--- tests/test_ffi.py
+++ tests/test_ffi.py
@@ -1,4 +1,5 @@
 import errno
+import os
 
 import pyprctl
 
@@ -27,3 +28,16 @@ def test_build_oserror() -> None:
     assert str(
         pyprctl.ffi.build_oserror(errno.EINVAL, "a", "b")
     ) == "[Errno {}] Invalid argument: 'a' -> 'b'".format(errno.EINVAL)
+
+
+def test_setresid_same() -> None:
+    uid = os.getuid()
+    gid = os.getgid()
+
+    # These calls will succeed without changing anything
+
+    pyprctl.ffi.sys_setresuid(-1, -1, -1)
+    pyprctl.ffi.sys_setresuid(uid, uid, uid)
+
+    pyprctl.ffi.sys_setresgid(-1, -1, -1)
+    pyprctl.ffi.sys_setresgid(gid, gid, gid)
