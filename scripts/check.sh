e5928fe8cc1f7f5993b16f1be13faf7e1462b930
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-01-02 21:53:13 -0500

    meta: remove pytype from "check" script
    pytype has been the source of several headaches, and it doesn't support
    Python 3.9.

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd "$(dirname -- "$(dirname -- "$(readlink -f "$0")")")"
 
-for cmd in flake8 isort mypy pylint pytype; do
+for cmd in flake8 isort mypy pylint; do
     if [[ ! -x "$(which "$cmd")" ]]; then
-        echo "Could not find $cmd. Please make sure that flake8, isort, mypy, pylint, and pytype are all installed."
+        echo "Could not find $cmd. Please make sure that flake8, isort, mypy, and pylint are all installed."
         exit 1
     fi
 done
 
-flake8 pyprctl tests && isort --check pyprctl tests && mypy --strict -p pyprctl -p tests && pytype pyprctl tests && pylint pyprctl tests
+flake8 pyprctl tests && isort --check pyprctl tests && mypy --strict -p pyprctl -p tests && pylint pyprctl tests
