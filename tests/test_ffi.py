d6ba6d72882a9ff43c82c77488c7854e3e775490
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-07-10 19:30:36 -0400

    test: add test for errors from setres*id()

--- tests/test_ffi.py
+++ tests/test_ffi.py
@@ -45,6 +45,30 @@ def test_setresid_same() -> None:
     pyprctl.ffi.sys_setresgid(gid, gid, gid)
 
 
+def test_setresid_error() -> None:
+    uid = os.getuid()
+    gid = os.getgid()
+
+    # Drop CAP_SETUID and CAP_SETGID if we have them
+    orig_state = pyprctl.CapState.get_current()
+    state = orig_state.copy()
+    state.effective.discard(pyprctl.Cap.SETUID)
+    state.effective.discard(pyprctl.Cap.SETGID)
+    state.set_current()
+
+    # This makes sure we're using the 32-bit UID/GID syscalls
+
+    with pytest.raises(OSError) as exc_info:
+        pyprctl.ffi.sys_setresuid(uid | 0xFFFF0000, uid | 0xFFFF0000, uid | 0xFFFF0000)
+    assert exc_info.value.errno in (errno.EINVAL, errno.EPERM)
+
+    with pytest.raises(OSError) as exc_info:
+        pyprctl.ffi.sys_setresgid(gid | 0xFFFF0000, gid | 0xFFFF0000, gid | 0xFFFF0000)
+    assert exc_info.value.errno in (errno.EINVAL, errno.EPERM)
+
+    orig_state.set_current()
+
+
 def test_setgroups_error() -> None:
     orig_state = False
     if pyprctl.cap_effective.setgid:
